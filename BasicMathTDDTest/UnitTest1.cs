699a65382e166f817d2fbc537f5d2baf1206939e
CuongM02 <cuongbg022002@gmail.com>
2024-03-13 11:05:42 +0700

    main

---
+++ BasicMathTDDTest/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using BasicMath;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace BasicMathTDDTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test_AddMethod()
+        {
+            BasicMaths bm = new BasicMaths();
+            double res = bm.Add(20.50, 20.50);
+            Assert.AreEqual(41, res);
+        }
+        [TestMethod]
+        public void Test_SubstractMethod()
+        {
+            BasicMaths bm = new BasicMaths();
+            double res = bm.Substract(10, 10);
+            Assert.AreEqual(res, 0);
+        }
+        [TestMethod]
+        public void Test_MultiplyMethod()
+        {
+            BasicMaths bm = new BasicMaths();
+            double res = bm.Multiply(10, 10);
+            Assert.AreEqual(100, res);
+        }
+        [TestMethod]
+        public void Test_DivideMethod()
+        {
+            BasicMaths bm = new BasicMaths();
+            double res = bm.divide(10, 5);
+            Assert.AreEqual(res, 2);
+        }
+    }
+}
